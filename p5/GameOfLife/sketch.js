61877f4c932e181dbf8969d7014e7633fad75917
jyaacoub <50300488+jyaacoub@users.noreply.github.com>
2020-07-01 01:18:31 -0400

    Grid setup

--- p5/GameOfLife/sketch.js
+++ p5/GameOfLife/sketch.js
@@ -1,18 +1,61 @@
+const TILE_SIZE = 20; // pixel size of each tile. 
+let grid;
+const rows = 42;
+const cols = 75;
+
 function createGrid(cols, rows){
 	// This function creates the 2D array that will represent the grid
 	let array2D = new Array(cols);
-
 	for (let i = 0; i < array2D.length; i++) {
 		array2D[i] = new Array(rows).fill(0);
 	}
 	return array2D;
 }
 
-let grid;
+function randomizeGrid(grid, probability){
+	// Fills the grid with random 1s and 0 according to a probabilty
+
+	//TODO: Finish randomizer for Grid.
+
+
+}
+
+function displayGrid(grid){
+	// This function is to display the grid.
+	stroke(0,255,0);
+
+	// Adds gridlines:
+	let width = grid.length * TILE_SIZE;
+	let height = grid[0].length * TILE_SIZE;
+	
+	// vertical lines
+	for (let i = 0; i < width; i+=TILE_SIZE) {
+		line(i, 0, i, height);
+	}
+
+	//horizontal lines
+	for (let i = 0; i < height; i+=TILE_SIZE) {
+		line(0, i, width, i);
+	}
+
+	// fills in the tiles according to the grid numbers
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[i].length; j++) {
+			// 1 means the tile is active/alive
+			if (grid[i][j]){
+			} else{ // 0 means the tile is disabled/dead
+			}
+			// TODO: add fill tile functionality.
+		}
+	}
+}
 
 function setup() {
-	grid = createGrid(3,4);
-	grid.forEach(x => console.log(x));
+	createCanvas(cols*TILE_SIZE, rows*TILE_SIZE);
+	background(100);
+	grid = createGrid(cols, rows);
+	grid = randomizeGrid();
+	displayGrid(grid);
 }
 
 function draw() {
