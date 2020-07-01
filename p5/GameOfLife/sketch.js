7485a125f20abe592b8161a4b403e2a10a93b1b4
jyaacoub <50300488+jyaacoub@users.noreply.github.com>
2020-07-01 19:11:23 -0400

    Finalized the grid

--- p5/GameOfLife/sketch.js
+++ p5/GameOfLife/sketch.js
@@ -1,60 +1,83 @@
-const TILE_SIZE = 20; // pixel size of each tile. 
+const TILE_SIZE = 20; 	// pixel size of each tile. 
+const ALIVE_PROB = 0.1; 	// The random-"alive" threshold.
+
+const COLS = 75;
+const ROWS = 42;
+
 let grid;
-const rows = 42;
-const cols = 75;
 
 function createGrid(cols, rows){
 	// This function creates the 2D array that will represent the grid
-	let array2D = new Array(cols);
+	let array2D = new Array(rows);
 	for (let i = 0; i < array2D.length; i++) {
-		array2D[i] = new Array(rows).fill(0);
+		array2D[i] = new Array(cols).fill(0);
 	}
 	return array2D;
 }
 
-function randomizeGrid(grid, probability){
+function randomizeGrid(grid){
 	// Fills the grid with random 1s and 0 according to a probabilty
-
-	//TODO: Finish randomizer for Grid.
-
-
+	for (let c = 0; c < grid.length; c++) {
+		for (let r = 0; r < grid[c].length; r++) {
+			randNum = Math.random();
+			if (randNum < ALIVE_PROB) { 
+				// If the "alive" threshold is met then the tile is active
+				grid[c][r] = 1;
+			} else{
+				grid[c][r] = 0;
+			}
+		}
+	}
+	return grid;
 }
 
 function displayGrid(grid){
 	// This function is to display the grid.
-	stroke(0,255,0);
-
-	// Adds gridlines:
-	let width = grid.length * TILE_SIZE;
-	let height = grid[0].length * TILE_SIZE;
-	
-	// vertical lines
-	for (let i = 0; i < width; i+=TILE_SIZE) {
-		line(i, 0, i, height);
-	}
-
-	//horizontal lines
-	for (let i = 0; i < height; i+=TILE_SIZE) {
-		line(0, i, width, i);
-	}
+	stroke(0);
 
 	// fills in the tiles according to the grid numbers
-	for (let i = 0; i < grid.length; i++) {
-		for (let j = 0; j < grid[i].length; j++) {
+	for (let c = 0; c < grid[0].length; c++) {
+		for (let r = 0; r < grid.length; r++) {
+			let x1 = c*TILE_SIZE;
+			let y1 = r*TILE_SIZE;
+
+			let x2 = x1 + TILE_SIZE;
+			let y2 = y1 + TILE_SIZE;
+
+			// let s = c+","+r;
+
 			// 1 means the tile is active/alive
-			if (grid[i][j]){
-			} else{ // 0 means the tile is disabled/dead
+			if (grid[r][c]){
+				fill(153);
+				rect(x1,y1,x2,y2);
+				
+				// fill(255,0,0);
+				// text(s, x1, y1, x2, y2);
+				// textAlign();
+
+				strokeWeight(2);
+				point(x1 + TILE_SIZE/2, y1 + TILE_SIZE/2);
+			}else {
+				fill(225);
+				rect(x1,y1,x2,y2);
+				
+				// fill(255,0,0);
+				// text(s, x1, y1, x2, y2);
+				// textAlign();
+				
+				strokeWeight(2);
+				point(x1 + TILE_SIZE/2, y1 + TILE_SIZE/2);
 			}
-			// TODO: add fill tile functionality.
+			strokeWeight(1);
 		}
 	}
 }
 
 function setup() {
-	createCanvas(cols*TILE_SIZE, rows*TILE_SIZE);
-	background(100);
-	grid = createGrid(cols, rows);
-	grid = randomizeGrid();
+	createCanvas(COLS*TILE_SIZE, ROWS*TILE_SIZE);
+	grid = createGrid(COLS, ROWS);
+	grid = randomizeGrid(grid);
+	grid.forEach(x => console.log(x));
 	displayGrid(grid);
 }
 
